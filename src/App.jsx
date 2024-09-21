946b6aba6d289c8dee3b7af7e04f526df21d1b91
Trainer <admin@example.com>
2024-09-21 18:24:57 +0100

    Intial Code.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Application() {
+  return <div style={{ backgroundColor: "blue", minHeight: "100px" }}>
+    hello world
+  </div>;
+}
+
+export default Application;
